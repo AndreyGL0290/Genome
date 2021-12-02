79a450cc1b32d9a9c459f21e14e19df8030e4260
Andrew <72341345+AndreyGL0290@users.noreply.github.com>
2021-12-02 16:57:35 +0700

    Add files via upload

--- Genome.py
+++ Genome.py
@@ -1,6 +1,7 @@
-from matplotlib.animation import FuncAnimation
+from random import choice, random
+
 from matplotlib import pyplot as plt
-from random import random, choice
+from matplotlib.animation import FuncAnimation
 
 '''
 Задать гены последовательностью 2 кода
@@ -11,30 +12,84 @@ CONFIG
 size = 128 * 128
 creatures = 10
 '''
-
+# Example gen - 00010203 04050607 08090A0B 0C0D0E0F
 creatures = 50
 creatures_list = {}
-rules = [bin(0), bin(1), bin(2), bin(3)]
+gens = [hex(i) for i in range(16*16)]
+def create_gen():
+    counter = 0
+    gen = ''
+    while counter <= 4:
+        gen += f'{choice(gens)} '
+        counter += 1
+    return gen
 
 def make_creature(i):
     x, y = round(128 * random()), round(128 * random())
-    creatures_list[i] = {'x': x, 'y': y, 'gen': choice(rules)}
-    return plt.plot(x, y, marker='o')
+    creatures_list[i] = {'x': x, 'y': y, 'gen': create_gen()}
+    return plt.scatter(x, y, marker='o', s=5)
 
 
 def make_population():
     for i in range(creatures):
         make_creature(i)
 
-def creatures_movement(i):
+survived_cretures = {}
+def choose_creatures():
+    '''Here we say what factor will be killing our little creatures'''
     for k, v in creatures_list.items():
-        for kk, vv in v.items():
-#             Здесь сделать перемещение (блок сравнения генов и далее движение путем замены координат в "массиве")
+        if v['x'] <= 64:
+            survived_cretures[k] = v
+    return survived_cretures
+
+x1 = []
+y1 = []
+def creatures_movement(i):
+    global x1, y1
+    plusX = 0
+    plusY = 0
+    if i <= 100:
+        for k, v in creatures_list.items():
+            # Change elifs
+            genes = creatures_list[k]['gen'].split()
+            if hex(0) in genes:
+                plusX = 1
+                plusY = 0
+            elif hex(46) in genes:
+                plusX = -1
+                plusY = 0
+            elif hex(30) in genes:
+                plusX = 0
+                plusY = 1
+            elif hex(28) in genes:
+                plusX = 0
+                plusY = -1
+            # size - plusX or plusY
+            if 2 <= creatures_list[k]['x'] <= 126:
+               creatures_list[k]['x'] += plusX
+            if 2 <= creatures_list[k]['y'] <= 126:
+                creatures_list[k]['y'] += plusY
+            plt.clf()
+            x1.insert(int(k), creatures_list[k]['x'])
+            del x1[creatures::]
+            y1.insert(int(k), creatures_list[k]['y'])
+            del y1[creatures::]
+        for i in range(creatures):
+            plt.scatter(x1[i], y1[i], marker='o', s=5)
+        plt.plot([0, 0, 128, 128, 0], [0, 128, 128, 0, 0], color='black')
+    else:
+        reproduction(choose_creatures())
+
+def reproduction(arr):
+    '''Here we cross parentes gens and making their childes'''
+    for k, v in arr.items():
+        pass
+    return
 
 make_population()
-ani = FuncAnimation(plt.gcf(), creatures_movement, interval=1000)
+ani = FuncAnimation(plt.gcf(), creatures_movement, interval=100, frames=102, repeat=False)
 
 
-plt.grid(b=True)
-print(creatures_list)
+plt.grid()
+
 plt.show()
