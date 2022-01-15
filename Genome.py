f8c82eb1c6f05065ebb664fa14de6dd8f0bc4e75
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-15 16:56:14 +0700

    Added reproduction and mutation features

--- Genome.py
+++ Genome.py
@@ -1,28 +1,19 @@
 from random import choice, random
-from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+from os import mkdir, getcwd, rmdir, remove
+from Adaptation.setup import size, creatures, creatures_list, k, max_population, gens, mutation_delay
+from Adaptation.children import ChildCreature, mutation
+from sys import setrecursionlimit
 
-'''
-Задать гены последовательностью 16 кода
-'''
+setrecursionlimit(10000)
 
-'''
-CONFIG
-size = 128 * 128
-creatures = 50
-'''
-# Example gen - 00010203 04050607 08090A0B 0C0D0E0F
-creatures = 50
-creatures_list = {}
+# Example gen - 00010203
 
-# Creating 256 possible gens
-gens = [hex(i) for i in range(16*16)]
-
-# This class creating cretures with different propertys
+# This class creating cretures with different properties
 class Creature:
     def __init__(self):
-        self.X = round(random() * 128)
-        self.Y = round(random() * 128)
+        self.X = round(random() * size)
+        self.Y = round(random() * size)
         self.gen = self.genom()
 
     @staticmethod
@@ -34,42 +25,98 @@ class Creature:
             counter += 1
         return gen
 
+    # Function that is responsible for movement of a SINGLE creature
+    # K here means coefficient
     def movement(self):
         genes = self.gen.split()
-        if hex(0) in genes and 2 <= self.X <= 126:
-            self.X += 1
-        elif hex(46) in genes and 2 <= self.X <= 126:
-            self.X += -1
-        elif hex(30) in genes and 2 <= self.Y <= 126:
-            self.Y += 1
-        elif hex(28) in genes and 2 <= self.Y <= 126:
-            self.Y += -1
+        if hex(0) in genes:
+            self.X += 1 * k
+            if self.X > size - 1:
+                self.X = size - 1
+        elif hex(4) in genes:
+            self.X += -1 * k
+            if self.X < 1:
+                self.X = 1
+        elif hex(8) in genes:
+            self.Y += 1 * k
+            if self.Y > size - 1:
+                self.Y = size - 1
+        elif hex(12) in genes:
+            self.Y += -1 * k
+            if self.Y < 1:
+                self.Y = 1
 
-def create_population():
-    for i in range(creatures):
-        creatures_list[i] = Creature()
-        plt.scatter(creatures_list[i].X, creatures_list[i].Y, marker='o', s=5)
+index = 0
+def create_population(population, shots):
+    global index
+    if population != max_population:
+        plt.clf()
+        if population == 0:
+            for i in range(creatures):
+                creatures_list[i] = Creature()
+                plt.scatter(creatures_list[i].X, creatures_list[i].Y, marker='o', s=5)
+        else:
+            for i in range(creatures):
+                plt.scatter(creatures_list[i].X, creatures_list[i].Y, marker='o', s=5)
+        plt.plot([0, 0, size, size, 0], [0, size, size, 0, 0], color='black')
+        try:
+            if population == shots[index]:
+                try:
+                    remove(f'{getcwd()}\\populations\\population №{population}\\start.png')
+                    remove(f'{getcwd()}\\populations\\population №{population}\\end.png')
+                    rmdir(f'{getcwd()}\\populations\\population №{population}')
+                except FileNotFoundError:
+                    pass
+                mkdir(f'{getcwd()}\\populations\\population №{population}')
+                plt.savefig(f'populations\\population №{population}\\start.png')
+        except IndexError:
+            pass
+        movement(population, shots)
 
-def movement(i):
+# Function that is responsible for movement of EVERY creature
+def movement(population, shots):
+    global index
     plt.clf()
     # Drawing bounds of our little world
-    plt.plot([0, 0, 128, 128, 0], [0, 128, 128, 0, 0], color='black')
+    plt.plot([0, 0, size, size, 0], [0, size, size, 0, 0], color='black')
     # Updating coordinates
     for i in range(creatures):
         creatures_list[i].movement()
         plt.scatter(creatures_list[i].X, creatures_list[i].Y, marker='o', s=5)
+    try:
+        if population == shots[index]:
+            plt.savefig(f'populations\\population №{population}\\end.png')
+            index += 1
+    except IndexError:
+        pass
+    rem = selection()
+    creatures_list.clear()
+    reproduction(rem)
+    create_population(population + 1, shots)
 
 remaining_creatures = {}
 def selection():
     for i in range(creatures):
-        if creatures_list[i].X <= 64:
-            remaining_creatures[i] = creatures_list[i]
+        if creatures_list[i].X <= size / 2:
+            remaining_creatures[len(remaining_creatures)] = creatures_list[i]
     return remaining_creatures
 
+newburns = 0
+def reproduction(rem):
+    global newburns
+    for i in range(len(rem) - 1):
+        if i % 2 == 1:
+            continue
+        creatures_list[len(creatures_list)] = ChildCreature(rem[i].gen.split(), rem[i + 1].gen.split())
+        newburns += 1
+        if newburns == mutation_delay:
+            mutation(creatures_list[len(creatures_list)-1])
+            newburns = 0
+        if len(creatures_list) == 50:
+            return
+    if len(creatures_list) != 50:
+        reproduction(rem)
 
-create_population()
-
-ani = FuncAnimation(plt.gcf(), movement, interval=100, frames=101, repeat=False)
-
-plt.grid(b=True)
-plt.show()
+if __name__ == '__main__':
+    # [i for i in range(150) if (i - 9) % 10 == 0 or i == 1] - every 10th
+    create_population(0, (0, 249, 499))
